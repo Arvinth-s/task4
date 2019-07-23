d44694598190049142a2274271102aef707fbb9d
Arvinth-s <immortalarvinths@gmail.com>
2019-07-23 23:20:55 +0530

    PI question is added as PI_question

---
+++ PI_question.cpp
@@ -0,0 +1,29 @@
+//OM NAMO NARAYANA
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	int i, count = 0;
+	cin>>i;
+	vector<int> index;
+	while(i / 2 > 0)
+	{
+		if(i % 2 == 1)
+			index.push_back(count);
+		count++;
+		i /= 2;
+	}
+	if(i%2 == 1)
+		index.push_back(count), count++;
+	int sum  = 0; 
+	vector<int>::iterator itr;
+	for(itr = index.end() - 1; itr != index.begin(); itr--)
+		sum += pow(2, *itr)*(*itr) + 1, cout<<sum<<"\t";
+	cout<<endl;
+	cout<<"ans:"<<sum;
+}
